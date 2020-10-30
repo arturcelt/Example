9ce06214398cd127f7f1b5a686d584a975fd285e
Artur Dybysławski <adybyslawski@hotmail.com>
2020-10-30 22:27:31 +0100

    REV_STANDARD

--- ClientApp/src/app/iterator.directive.ts
+++ ClientApp/src/app/iterator.directive.ts
@@ -1,4 +1,6 @@
-import { Directive, ViewContainerRef, TemplateRef, Input, SimpleChange, IterableDiffer, IterableDiffers, ChangeDetectorRef, CollectionChangeRecord, DefaultIterableDiffer } from "@angular/core";
+import {
+  Directive, ViewContainerRef, TemplateRef, Input, SimpleChange, IterableDiffer, IterableDiffers, ChangeDetectorRef, CollectionChangeRecord, DefaultIterableDiffer, ViewRef
+} from "@angular/core";
 
 
 @Directive({
@@ -6,6 +8,7 @@ import { Directive, ViewContainerRef, TemplateRef, Input, SimpleChange, Iterable
 })
 export class PaIteratorDirective {
   private differ: DefaultIterableDiffer<any>;
+  private views: Map<any, PaIteratorContext> = new Map<any, PaIteratorContext>();
 
   constructor(private container: ViewContainerRef, private template: TemplateRef<Object>, private differs: IterableDiffers, private changeDetector: ChangeDetectorRef) { }
 
@@ -29,8 +32,25 @@ export class PaIteratorDirective {
     if (changes != null) {
       console.log("Wywołanie metody ngDoCheck(), wykryto zmiany.");
       changes.forEachAddedItem(addition => {
-        this.container.createEmbeddedView(this.template, new PaIteratorContext(addition.item, addition.currentIndex, changes.length));
+        let context = new PaIteratorContext(addition.item, addition.currentIndex, changes.length);
+        context.view = this.container.createEmbeddedView(this.template, context);
+        this.views.set(addition.trackById, context);
+
+        //this.container.createEmbeddedView(this.template, new PaIteratorContext(addition.item, addition.currentIndex, changes.length));
       });
+      let removals = false;
+      changes.forEachRemovedItem(removal => {
+        removals = true;
+        let context = this.views.get(removal.trackById);
+        if (context != null) {
+          this.container.remove(this.container.indexOf(context.view));
+          this.views.delete(removal.trackById);
+        }
+      });
+      if (removals) {
+        let index = 0;
+        this.views.forEach(context => context.setData(index++, this.views.size));
+      }
     }
   }
 
@@ -44,16 +64,18 @@ export class PaIteratorDirective {
 }
 
 class PaIteratorContext {
+  index: number;
   odd: boolean;
   even: boolean;
   first: boolean;
   last: boolean;
+  view: ViewRef;
 
-  constructor(public $implicit: any, public index: number, total: number) {
-    this.odd = index % 2 == 1;
-    this.even = !this.odd;
-    this.first = index == 0;
-    this.last = index == total - 1;
+  constructor(public $implicit: any, public position: number, total: number) {
+
+    this.setData(position, total);
+
+    
 
     //setInterval(() => {
     //  this.odd = !this.odd;
@@ -61,4 +83,14 @@ class PaIteratorContext {
     //  this.$implicit.price++;
     //}, 2000);
   }
+
+
+  setData(index: number, total: number) {
+    this.index = index;
+    this.odd = index % 2 == 1;
+    this.even = !this.odd;
+    this.first = index == 0;
+    this.last = index == total - 1;
+  }
+
 }
