73361248cba3d928b8416c827bb31919bf148680
Artur Dybysławski <adybyslawski@hotmail.com>
2020-11-05 20:07:07 +0100

    REV_20.1

---
+++ ClientApp/src/app/log.service.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var LogLevel;
+(function (LogLevel) {
+    LogLevel[LogLevel["DEBUG"] = 0] = "DEBUG";
+    LogLevel[LogLevel["INFO"] = 1] = "INFO";
+    LogLevel[LogLevel["ERROR"] = 2] = "ERROR";
+})(LogLevel = exports.LogLevel || (exports.LogLevel = {}));
+//# sourceMappingURL=log.service.js.map
