56c5cf6220df3e149dfd5ceb018649112c1913b4
Artur Dybysławski <adybyslawski@hotmail.com>
2020-11-05 19:10:36 +0100

    REV_19.28

--- ClientApp/src/app/productTable.component.ts
+++ ClientApp/src/app/productTable.component.ts
@@ -9,8 +9,10 @@ import { DiscountService } from "./discount.service";
 })
 export class ProductTableComponent {
   //discounter: DiscountService = new DiscountService();
-  @Input("model")
-  dataModel: Model;
+  //@Input("model")
+  //dataModel: Model;
+
+  constructor(private dataModel: Model) { }
 
   getProduct(key: number): Product {
     return this.dataModel.getProduct(key);
