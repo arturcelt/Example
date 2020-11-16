f6cbcacf5374273bee5ad47bfac3f82532bae4af
Artur Dybysławski <adybyslawski@hotmail.com>
2020-11-16 20:40:15 +0100

    REV_21.10

--- ClientApp/src/app/attr.directive.ts
+++ ClientApp/src/app/attr.directive.ts
@@ -1,33 +1,5 @@
-//import { Directive, ElementRef, Attribute } from "@angular/core";
-
-//@Directive({
-//  selector: "[pa-attr]"
-//})
-//export class PaAttrDirective {
-//  constructor(element: ElementRef, @Attribute("pa-attr") bgClass: string) {
-//    element.nativeElement.classList.add(bgClass || "bg-success");
-//  }
-//}
-
-//import { Directive, ElementRef, Attribute, Input } from "@angular/core";
-
-//@Directive({
-//  selector: "[pa-attr]"
-//})
-//export class PaAttrDirective {
-//  constructor(private element: ElementRef) { }
-
-//  @Input("pa-attr")
-//  bgClass: string;
-
-//  ngOnInit() {
-//    this.element.nativeElement.classList.add(this.bgClass || "bg-success");
-//  }
-//  }
-
-
 import { Directive, ElementRef, Attribute, Input, SimpleChange, Output, EventEmitter, HostListener, HostBinding } from "@angular/core";
-import { Product } from "./product.model";
+import { Product } from "./model/product.model";
 
 @Directive({
   selector: "[pa-attr]"
@@ -58,19 +30,4 @@ export class PaAttrDirective {
     }
   }
 
-  //ngOnInit() {
-  //  this.element.nativeElement.classList.add(this.bgClass || "bg-success");
-  //}
-
-  //ngOnChanges(changes: { [property: string]: SimpleChange }) {
-  //  let change = changes["bgClass"];
-  //  let classList = this.element.nativeElement.classList;
-
-  //  if (!change.isFirstChange() && classList.contains(change.previousValue)) {
-  //    classList.remove(change.previousValue);
-  //  }
-  //  if (!classList.contains(change.currentValue)) {
-  //    classList.add(change.currentValue);
-  //  }
-  //}
 }
